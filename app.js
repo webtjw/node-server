c0f3aa440c6b69d1c95d06ed12fc7c530b35429f
jiawei <webtjw@foxmail.com>
2017-10-02 22:51:23 +0800

    koa-router,Sequelize

--- app.js
+++ app.js
@@ -6,29 +6,20 @@ const Koa = require('koa');
 const Router = require('koa-router');
 
 const app = new Koa();
-const router = new Router();
+const router = new Router({prefix: '/api'});
 
-
-// 中间件列表
-app.use(async (context, next) => {
-  console.log(1);
-  await next();
-})
-
-app.use(async (context, next) => {
-  console.log(2);
-  await next();
-  console.log(4);
-})
-
-app.use(async (context, next) => {
-  console.log(3);
-  await next();
-  console.log(5);
+router.all('/jiawei', (context, next) => {
+  console.log('jiawei');
 })
 
 // custom variable
 const port = 8000;
+app.use(router.routes());
+
+app.on('error', err => {
+  log.error('server error', err)
+});
+
 app.listen(port);
 
 // log info
