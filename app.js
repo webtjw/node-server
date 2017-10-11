3b8b758e41b917f3f7779a6d0714d591b3f1dcdc
jiawei <webtjw@foxmail.com>
2017-10-11 21:18:58 +0800

    add router-initialization

--- app.js
+++ app.js
@@ -3,29 +3,10 @@ const path = require('path'),
 
 global.RootPath = path.resolve('');
 
-const Koa = require('koa');
 const app = new Koa();
 
-// routes
-const indexRouter = require('./routes/index');
-app.use(indexRouter.routes());
-const apiRouter = require('./routes/api');
-app.use(apiRouter.routes());
-// static
-app.use(require('koa-static')(__dirname + '/static', {
-  maxage: 1000 * 60 * 60 * 24 * 1
-}));
-// 404
-app.use(async (ctx, next) => {
-  console.log('===> into 404');
-  ctx.response.status = 404;
-  ctx.redirect('/404.html');
-})
-
-// error handle
-app.on('error', err => {
-  console.error('server error', err)
-});
+// router init
+require('./routes/initialize').init(app);
 
 const port = 3000;
 app.listen(port);
--- routes/initialize.js
+++ routes/initialize.js
@@ -1,19 +1,40 @@
-// normal routes
-const indexRouter = require('./index');
-app.use(indexRouter.routes());
-const apiRouter = require('./api');
-app.use(apiRouter.routes());
-// static
-app.use(require('koa-static')(__dirname + '/static', {
-  maxage: 1000 * 60 * 60 * 24 * 1
-}));
-// 404
-app.use(async (ctx, next) => {
-  console.log('===> into 404');
-  ctx.response.status = 404;
-  ctx.redirect('/404.html');
-})
+const path = require('path');
+
 const RouterKit = {
-  
+  app: null,
+  init (server) {
+    if (!server) throw new TypeError('wrong server in router initialization.');
+
+    this.app = server;
+    // normal routes
+    this.initNormalRouter();
+    // static resources
+    this.initStatics();
+    // 404 page
+    this.init404();
+    // error
+    this.app.on('error', this.errorDealing);
+  },
+  initNormalRouter () {
+    const normalRoutesList = [
+      require('./index'),
+      require('./api')
+    ];
+    normalRoutesList.map(item => this.app.use(item.routes()))
+  },
+  init404 () {
+    this.app.use(async (ctx, next) => {
+      ctx.response.status = 404;
+      ctx.redirect('/404.html');
+    })
+  },
+  initStatics () {
+    this.app.use(require('koa-static')(path.resolve(__dirname, '../static'), {
+      maxage: 1000 * 60 * 60 * 24 * 1 // 1 day
+    }));
+  },
+  errorDealing (error) {
+    console.error('server error ===> ', err);
+  }
 };
 module.exports = RouterKit;
