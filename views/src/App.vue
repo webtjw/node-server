be492988999af06b8476039a18d9d4de0278846e
jiawei <webtjw@foxmail.com>
2017-10-12 23:30:07 +0800

    init header

--- views/src/App.vue
+++ views/src/App.vue
@@ -1,23 +1,21 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <top-bar></top-bar>
+    <router-view />
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  import TopBar from './components/common/TopBar'
+  
+  export default {
+    name: 'app',
+    components: {
+      TopBar
+    }
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ views/src/components/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="w-1000">
+    index
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Index'
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
