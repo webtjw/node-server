4032ddd22127457cbc1d64eb8c7a01b9de505ce7
jiawei <webtjw@foxmail.com>
2018-01-25 09:46:46 +0800

    使用命令行参数区分开发生产环境

--- app.js
+++ app.js
@@ -10,8 +10,10 @@ app.use(require('koa-bodyparser')());
 // router init
 require('./routes/initialize').init(app);
 
-const port = 3000;
-app.listen(port);
+
+const cmdArgs = process.argv.splice(2);
+if (cmdArgs.length === 1 && cmdArgs[0] === 'dev') app.listen(3000);
+else app.listen(80);
 
 // log info
 console.log(`Server running at http://127.0.0.1:${port}/`);
