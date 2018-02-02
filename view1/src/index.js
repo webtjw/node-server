17f73908ab190ccef2cca61922c7420524937e5b
jiawei <webtjw@foxmail.com>
2018-02-02 11:18:52 +0800

    css built in dev & pro

--- view1/src/index.js
+++ view1/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './assets/style/app-reset.css';
+import './assets/style/common.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
