8d92b2c229bf5482dff18e150e0a97811ebde63a
jiawei <webtjw@foxmail.com>
2018-02-23 22:28:06 +0800

    topbar match routes

--- view3/src/components/common/Topbar/Topbar.vue
+++ view3/src/components/common/Topbar/Topbar.vue
@@ -2,9 +2,9 @@
   <div id="topbar">
     <div class="wrapper" flex="dir:left main:right cross:center">
       <nav flex="dir:left">
-        <router-link class="nav-item m-l-12 p-h-8 p-v-8 font-14" v-for="nav in navList" :key="nav.name" :to="nav.path" flex="dir:left cross:center">
-          <vue-svg :name="nav.icon" class="svg-14 m-r-6"></vue-svg>
-          <span>{{nav.name}}</span>
+        <router-link class="nav-item m-l-4 p-v-6 font-14" :class="{selected: index === navIndex}" v-for="(nav, index) in navList" :to="nav.path" :key="nav.name" @click="jump(nav.path, index)" flex="dir:left cross:center">
+          <!-- <vue-svg :name="nav.icon" class="svg-14 m-r-6"></vue-svg> -->
+          <span class="p-h-20">{{nav.name}}</span>
         </router-link>
       </nav>
     </div>
@@ -16,13 +16,36 @@
     data () {
       return {
         navList: [
-          {name: '首页', path: '/', icon: 'index'},
-          {name: '分类', path: '/category', icon: 'category'},
-          {name: '标签', path: '/tags', icon: 'tag'},
-          {name: '归档', path: '/archives', icon: 'archive'},
-          {name: '关于', path: '/about', icon: 'about'}
-        ]
+          {name: '首页', path: '/', prefix: 'index'},
+          {name: '分类', path: '/category', prefix: 'category'},
+          {name: '标签', path: '/tags', prefix: 'tags'},
+          {name: '归档', path: '/archives', prefix: 'archive'},
+          {name: '关于', path: '/about', prefix: 'about'}
+        ],
+        navIndex: 0
       }
+    },
+    methods: {
+      matchNavIndex (path) {
+        if (path === '/') this.navIndex = 0;
+        else {
+          var pathArray = path.split('/');
+          var prefix = pathArray[1];
+          prefix && this.navList.forEach((item, index) => {
+            if (prefix === item.prefix) {
+              this.navIndex = index;
+            }
+          })
+        }
+      }
+    },
+    watch: {
+      $route (obj) {
+        this.matchNavIndex(obj.path);
+      }
+    },
+    mounted () {
+      this.matchNavIndex(this.$route.path);
     }
   }
 </script>
@@ -36,11 +59,18 @@
       
       .nav-item {
         color: #333;
-        transition: all .3s ease-out;
         background-color: #fff;
-        &:hover {
-          background-color: #333;
-          color: #fff;
+        cursor: pointer;
+        transition: all .3s ease-out;
+        &:hover, &.selected {
+          color: #409EFF;
+        }
+        &:last-child span {
+            border: 0;
+        }
+
+        span {
+          border-right: 1px solid #999;
         }
       }
     }
