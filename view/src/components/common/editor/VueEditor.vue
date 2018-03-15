3464d218b5f9af09a36d7a5da68546532af413d7
jiawei <webtjw@foxmail.com>
2018-03-15 22:36:15 +0800

    原始 md 预处理 + title 功能初步实现

--- view/src/components/common/editor/VueEditor.vue
+++ view/src/components/common/editor/VueEditor.vue
@@ -3,7 +3,7 @@
     <!-- tools -->
     <div class="tools p-h-20 p-v-10 relative" flex="dir:left main:justify cross:center">
       <div class="edit-tools">
-        <vue-svg v-for="tool in editTools" :key="tool.icon" :icon="tool.icon" :title="tool.title" class="svg-18 pointer m-h-4 p-4"></vue-svg>
+        <vue-svg v-for="tool in editTools" :key="tool.icon" :icon="tool.icon" :title="tool.title" @click.native="tool.method" class="svg-18 pointer m-h-4 p-4"></vue-svg>
       </div>
       <div class="functional-tools">
         <vue-svg v-for="tool in functionalTools" :key="tool.icon" :icon="tool.icon" class="svg-18 pointer m-h-4 p-2"></vue-svg>
@@ -11,7 +11,7 @@
     </div>
     <!-- edit area -->
     <div class="edit-container" ref="edit" :style="{height: editHeight + 'px'}">
-      <textarea class="v-input-feild v-area-item" v-model="inputValue" ref="input" @scroll="sameScroll($event, 'right')" @mouseover="mouseScrollType = 0" spellcheck="false"></textarea>
+      <textarea class="v-input-feild v-area-item" v-model="inputValue" ref="input" @scroll="sameScroll($event, 'right')" @mouseover="mouseScrollType = 0" spellcheck="false" @select="updateSelection" @click="updateSelection" @keydown="updateSelection"></textarea>
       <pre class="preview v-area-item" ref="preview" v-html="compileHTML" @scroll="sameScroll($event, 'left')" @mouseover="mouseScrollType = 1">
         <!-- 暂时为了实现同屏效果采用 pre，后面要改回 div -->
       </pre>
@@ -21,25 +21,25 @@
 
 <script>
 import preProcess from './preProcess'
-import markdownIt from 'markdown-it'
+import MarkdownIt from 'markdown-it'
 import './importSvg'
-const markdown = markdownIt()
+const markdown = new MarkdownIt()
 
 export default {
   data () {
     return {
       editTools: [
-        {icon: 'svg-title', title: '设置为标题', method: null},
-        {icon: 'svg-bold', title: '粗体', method: null},
-        {icon: 'svg-underline', title: '下划线', method: null},
-        {icon: 'svg-center', title: '居中', method: null},
-        {icon: 'svg-quote', title: '引用', method: null},
-        {icon: 'svg-list', title: '列表', method: null},
-        {icon: 'svg-link', title: '插入链接', method: null},
-        {icon: 'svg-image', title: '插入图片', method: null},
-        {icon: 'svg-code', title: '插入代码段', method: null},
-        {icon: 'svg-codes', title: '插入代码块', method: null},
-        {icon: 'svg-table', title: '插入表格', method: null}
+        {icon: 'svg-title', title: '设置为标题', method: this.setTitle},
+        {icon: 'svg-bold', title: '粗体', method: this.setTitle},
+        {icon: 'svg-underline', title: '下划线', method: this.setTitle},
+        {icon: 'svg-center', title: '居中', method: this.setTitle},
+        {icon: 'svg-quote', title: '引用', method: this.setTitle},
+        {icon: 'svg-list', title: '列表', method: this.setTitle},
+        {icon: 'svg-link', title: '插入链接', method: this.setTitle},
+        {icon: 'svg-image', title: '插入图片', method: this.setTitle},
+        {icon: 'svg-code', title: '插入代码段', method: this.setTitle},
+        {icon: 'svg-codes', title: '插入代码块', method: this.setTitle},
+        {icon: 'svg-table', title: '插入表格', method: this.setTitle}
       ],
       functionalTools: [
         {icon: 'svg-undo', title: '撤销改动', method: null},
@@ -49,14 +49,23 @@ export default {
       ],
       editHeight: 0,
       inputValue: '',
-      mouseScrollType: -1
+      mouseScrollType: -1,
+      inputSelection: {
+        start: 0,
+        end: 0,
+        prev: '',
+        selected: '',
+        next: ''
+      }
     }
   },
   computed: {
     compileHTML () {
       const {inputValue} = this
       const articleStructure = preProcess(inputValue)
-      return articleStructure
+      const {title, whole} = articleStructure
+      const renderString = title || whole ? `# 标题 - ${articleStructure.title || ''} #\n${articleStructure.whole}` : ''
+      return markdown.render(renderString)
     }
   },
   methods: {
@@ -72,6 +81,32 @@ export default {
     },
     changeScroll (el, distance) {
       el.scrollTop = distance
+    },
+    updateSelection () {
+      const {$refs: {input}, inputValue} = this
+      const start = input.selectionStart
+      const end = input.selectionEnd
+      this.inputSelection.start = start
+      this.inputSelection.end = end
+      this.inputSelection.prev = inputValue.slice(0, start)
+      this.inputSelection.selected = inputValue.slice(start, end)
+      this.inputSelection.next = inputValue.slice(end)
+    },
+    setTitle () {
+      const {inputSelection: {start, selected, next}, $refs: {input}} = this
+      if (start === 0) {
+        this.inputValue = `#t ${(selected || '标题') + (next.startsWith('\n') ? '' : '\n')}` + next
+        this.$nextTick(() => {
+          input.selectionStart = 3
+          input.selectionEnd = 3 + (selected.length || 2)
+          input.focus()
+        })
+      }
+    }
+  },
+  watch: {
+    inputValue () {
+      this.updateSelection()
     }
   },
   mounted () {
--- view/src/components/common/editor/preProcess.js
+++ view/src/components/common/editor/preProcess.js
@@ -1,15 +1,23 @@
+const moreString = '<!-- more -->'
+
 export default function (markdown) {
   let article = {
-    title: null
+    title: null,
+    description: null,
+    whole: null
   }
-  // get ttitle
+  // get title
   const regTitle = markdown.match(/^#t\s+([^\n]+)\n*/)
   if (Array.isArray(regTitle) && regTitle[1]) {
     article.title = regTitle[1].replace(/\s*$/, '')
   }
-  // get description before 'more' boundary
-  if (markdown.indexOf('<!-- more -->') > -1) {
-    console.log('more')
-  }
-  return markdown
+  // get the article content and description which excludes title
+  let mdWithoutTitle = markdown
+  if (article.title) mdWithoutTitle = markdown.replace(/^#t[^\n]*(\n)*/, '')
+  const moreIndex = mdWithoutTitle.indexOf(moreString)
+  if (moreIndex > -1) {
+    article.description = mdWithoutTitle.slice(0, moreIndex)
+    article.whole = mdWithoutTitle.replace(moreString, '')
+  } else article.whole = mdWithoutTitle
+  return article
 }
