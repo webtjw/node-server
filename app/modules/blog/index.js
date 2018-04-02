017693560588629459e45233c25f4d0c71964bdb
jiawei <webtjw@foxmail.com>
2018-04-02 10:00:58 +0800

    fix bug of article detail

--- app/modules/blog/index.js
+++ app/modules/blog/index.js
@@ -41,6 +41,7 @@ const article = {
     if (indexResult.success) {
       indexResult.data.forEach(item => {
         item.tags = item.tags.split(',');
+        item.codeText = item.codeText.replace(/^#t\s+([^\n]+)\n*/, '');
       })
       httpKit.setResponse(ctx, {data: indexResult.data});
     }
@@ -53,6 +54,7 @@ const article = {
       const articleResult = await spots.getArticleById(id);
       const article = articleResult.data[0];
       article.tags = article.tags.split(',');
+      article.codeText = article.codeText.replace(/^#t\s+([^\n]+)\n*/, '');
       if (articleResult.success) httpKit.setResponse(ctx, {data: article});
     }
   },
