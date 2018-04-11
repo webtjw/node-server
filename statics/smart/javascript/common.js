963f09d8b1b7c590ed43f5cb30fa9413075360e9
jiawei <webtjw@foxmail.com>
2018-04-11 15:38:00 +0800

    delete extra files

--- statics/smart/javascript/common.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  // sidebar
-  $('#sidebar').on('click', '.sub-nav', function () {
-    var $this = $(this),
-      changeStatus = $this.attr('data-expand') === 'true' ? 'false' : 'true';
-
-    $this.attr('data-expand', changeStatus);
-  })
-})()
