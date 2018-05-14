31499e171d95054df6a3f1f05c31ece7a29cc06f
jiawei <webtjw@foxmail.com>
2018-05-14 10:22:12 +0800

    更改关于

--- view/src/components/About.vue
+++ view/src/components/About.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="m-v-40">
     <div class="p-t-40 sentence-list">
-      <p>Hi，我是大猫。</p>
+      <p>Hi，我是 Robin。</p>
       <p>email：<a href="mailto:webtjw@foxmail.com">webtjw@foxmail.com</a></p>
       <p>github：<a href="https://github.com/webtjw" target="_blank">webtjw</a></p>
     </div>
