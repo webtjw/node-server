141071acc81eb5c813f33b314a899be0e268e21f
jiawei <webtjw@foxmail.com>
2018-06-05 11:48:27 +0800

    recover to api server

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const Koa = require('koa')
 const koaBody = require('koa-body')
 const dispatchRoute = require('./app/routes/dispatchRoute')
 const config = require('./app/config')
-const ssrInitialize = require('./ssr/router')
 
 const app = new Koa() // app 实例
 
@@ -14,8 +13,8 @@ app.use(koaBody({multipart: true})) // formdata 文件上传
 app.use(require('koa-bodyparser')()) // 解析请求体
 
 /* 具名路由分发处理 */
-ssrInitialize(app)
-// dispatchRoute(app)
+dispatchRoute(app)
 
 /* 启动监听 */
+app.listen(config.port)
 console.log(`Server running at port ${config.port}.`)
