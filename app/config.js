d0b6e715dce15b4411bc8bf570b22a23b59e280a
jiawei <webtjw@foxmail.com>
2018-06-08 10:01:45 +0800

    port

--- app/config.js
+++ app/config.js
@@ -3,5 +3,5 @@ const isDevelopment = process.env.NODE_ENV === 'development'
 module.exports = {
   isDevelopment,
   isProduction: !isDevelopment,
-  port: isDevelopment ? 3001 : 80
+  port: 3001
 }
